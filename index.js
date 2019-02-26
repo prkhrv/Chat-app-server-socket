e640339419b942c632537d2d8e8915674a0783dc
Prakhar Varshney <prakharnegative@gmail.com>
2019-02-26 19:19:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ io.on("connection",(socket)=>{
 	  io.sockets.in(rooms[socket.id]).emit("chat message",msg);
 
 	  // *********************
-	  var new_chat_group = new Chat({_id:rooms[socket.id],messages:{username:"Computer",message:msg}});
+	  var new_chat_group = new Chat({_id:rooms[socket.id],messages:{username:"Computer",message:"hi"}});
     		new_chat_group.save()
 	  	// Chat.findOneAndUpdate({_id:rooms[socket.id]},{$push: {messages:{message:msg.message,username:msg.user}}},{new:true},function(err,chat_group){
 
