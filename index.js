eb1b9c904005067fd25959d2b084b6f84e367948
Prakhar Varshney <prakharnegative@gmail.com>
2019-02-26 19:05:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var app = require('express')();
-var port = 3000;
+var port = process.env.PORT || 3000,
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 var bodyParser = require('body-parser');
