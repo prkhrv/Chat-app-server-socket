a2dab90234ca4583670b153b5ffc584e3dd72b52
Prakhar Varshney <prakharnegative@gmail.com>
2019-04-11 15:06:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -121,6 +121,31 @@ app.get('/mobile/:room',function(req,res){
 
 });
 
+app.post('/mobile/notification',function(req,res){
+  var payload = {
+  notification: {
+    title: "TravelKit",
+    body: "Your Request has been Accepted",
+  },
+  token: req.body.user_token,
+};
+
+
+
+
+// Send a message to the device corresponding to the provided
+// registration token.
+admin.messaging().send(payload)
+  .then((response) => {
+    // Response is a message ID string.
+    console.log('Successfully sent message:', response);
+  })
+  .catch((error) => {
+    console.log('Error sending message:', error);
+  });
+
+});
+
 var users = 0;
 io.on("connection",(socket)=>{
 
